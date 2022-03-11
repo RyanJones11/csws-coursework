e2ec571b36b37e653f18247006b9ba4ff452c00d
Andrew Wallace <97959282+AndrewWallace1999@users.noreply.github.com>
2022-03-11 22:03:25 +0000

    Update PercentPassing.py
    Rewrote larger section of the code to make it repeatable and more efficient.

--- PercentPassing.py
+++ PercentPassing.py
@@ -1,6 +1,41 @@
 import pandas as pd
 
 def TotalPassing():
+
+    def PrintPassing(focus, i):
+        print(focus[i], end=": ")
+        print(value[i], end="%\n")
+
+    def CalcPassing(focus, myIndex):
+        # Gets total percentage of passing student and percentage of passing students based on ethnicity
+        variableKeys = focus.drop_duplicates(keep='first', inplace=False)
+        for x, valx in variableKeys.iteritems():
+            for i, val in ethnicity.iteritems():
+                if myIndex == 1:
+                    if ethnicity[i] == variableKeys[x] and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
+                        PrintPassing(ethnicity, i)
+                if myIndex == 2:
+                    if ethnicity[i] == "All" and gender[i] == variableKeys[x] and gender[i] != "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
+                        PrintPassing(gender, i)
+                if myIndex == 3:
+                    if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == variableKeys[x] and freeSchoolMeals[i] != "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
+                        PrintPassing(freeSchoolMeals, i)
+                if myIndex == 4:
+                    if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == variableKeys[x] and senType[i] != "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
+                        PrintPassing(senType, i)
+                if myIndex == 5:
+                    if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == variableKeys[x] and senGroup[i] != "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
+                        PrintPassing(senGroup, i)
+                if myIndex == 6:
+                    if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == variableKeys[x] and admissionType[i] != "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
+                        PrintPassing(admissionType, i)
+                if myIndex == 7:
+                    if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == variableKeys[x] and schoolCharacteristic[i] != "All state-funded" and religion[i] == "All":
+                        PrintPassing(schoolCharacteristic, i)
+                if myIndex == 8:
+                    if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == variableKeys[x] and religion[i] != "All":
+                        PrintPassing(religion, i)
+
     # Reads the data from file.
     data = pd.read_csv("data.csv")
 
@@ -15,68 +50,13 @@ def TotalPassing():
     religion = data["Religious_denomination"]
     value = data["Value"]
 
-    # Gets total percentage of passing student and percentage of passing students based on ethnicity
-    variableKeys = ethnicity.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in ethnicity.iteritems():
-            if ethnicity[i] == variableKeys[x] and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
-                print(ethnicity[i], end=": ")
-                print(value[i], end="%\n")
-
-    # Gender
-    variableKeys = gender.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in gender.iteritems():
-            if ethnicity[i] == "All" and gender[i] == variableKeys[x] and gender[i] != "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
-                print(gender[i], end=": ")
-                print(value[i], end="%\n")
-
-    # Free School Meals
-    variableKeys = freeSchoolMeals.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in freeSchoolMeals.iteritems():
-            if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == variableKeys[x] and freeSchoolMeals[i] != "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
-                print(freeSchoolMeals[i], end=": ")
-                print(value[i], end="%\n")
-
-    # SEN Type
-    variableKeys = senType.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in senType.iteritems():
-            if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == variableKeys[x] and senType[i] != "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
-                print(senType[i], end=": ")
-                print(value[i], end="%\n")
-
-    # SEN Group
-    variableKeys = senGroup.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in senGroup.iteritems():
-            if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == variableKeys[x] and senGroup[i] != "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
-                print(senGroup[i], end=": ")
-                print(value[i], end="%\n")
-
-    # Admission Type
-    variableKeys = admissionType.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in admissionType.iteritems():
-            if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == variableKeys[x] and admissionType[i] != "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == "All":
-                print(admissionType[i], end=": ")
-                print(value[i], end="%\n")
-
-    # School Characteristics
-    variableKeys = schoolCharacteristic.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in schoolCharacteristic.iteritems():
-            if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == variableKeys[x] and schoolCharacteristic[i] != "All state-funded" and religion[i] == "All":
-                print(schoolCharacteristic[i], end=": ")
-                print(value[i], end="%\n")
-
-    # Religion
-    variableKeys = religion.drop_duplicates(keep='first', inplace=False)
-    for x, valx in variableKeys.iteritems():
-        for i, val in schoolCharacteristic.iteritems():
-            if ethnicity[i] == "All" and gender[i] == "All" and freeSchoolMeals[i] == "All" and senType[i] == "All" and senGroup[i] == "All" and admissionType[i] == "All" and schoolCharacteristic[i] == "All state-funded" and religion[i] == variableKeys[x] and religion[i] != "All":
-                print(religion[i], end=": ")
-                print(value[i], end="%\n")
+    CalcPassing(ethnicity, 1)
+    CalcPassing(gender, 2)
+    CalcPassing(freeSchoolMeals, 3)
+    CalcPassing(senType, 4)
+    CalcPassing(senGroup, 5)
+    CalcPassing(admissionType, 6)
+    CalcPassing(schoolCharacteristic, 7)
+    CalcPassing(religion, 8)
 
 TotalPassing()
