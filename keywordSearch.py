201d9bf8e7c5fa8187edcdac17ef1ab36abad1d4
ryanajones1@hotmail.co.uk <ryanajones1@hotmail.co.uk>
2022-03-15 12:55:56 +0000

    Bug fixed the repeated error messages

--- keywordSearch.py
+++ keywordSearch.py
@@ -1,18 +1,19 @@
 import csv
 def keywordSearch():
     data = []
-    with open("dataset.csv") as csvfile:
+    searchResults = []
+    with open("dataset.csv") as csvfile:#'dataset.csv' to be replaced with the final filename of the dataset
         reader = csv.reader(csvfile)
         for row in reader:
             data.append(row)
     searchTerm = input("Please enter a search term:\n")
     for i in range(0,(len(row) - 4)):
         column = [x[i] for x in data]
-        #print(column)
         if searchTerm in column:
             for x in range(0,len(data)):
                 if searchTerm == data[x][i]:
-                    print(data[x])
-        else:
-            print("Error search term not found...")
+                    searchResults.append(data[x])
+                    print(searchResults)
+    if len(searchResults) == 0:
+        print("Error no results match \"" + searchTerm +"\"")
 keywordSearch()
