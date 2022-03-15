cb9fb893f683b64c30f92b5e17c7bf4529e7a00e
HarryMcAllen <hmcallen@outlook.com>
2022-03-15 14:17:22 +0000

    testing different methods

--- averages.py
+++ averages.py
@@ -3,24 +3,40 @@ from numpy import average
 
 data = pd.read_csv("data.csv")
 
+ethnicity = data["Ethnicity"]
 gender = data["Gender"]
+freeSchoolMeals = data["FSM"]
+senType = data["SEN_type"]
+senGroup = data["SEN_grouping"]
+admissionType = data["Admission_type"]
+schoolCharacteristic = data["School_characteristic"]
+religion = data["Religious_denomination"]
 value = data["Value"]
+denominator = data["Denominator"]
+numerator = data["Numerator"]
 
-
-sum = 0  
-sum2 = 0      
-boys = []
-for i, val in gender.iteritems():
-    if gender[i] == "Boys":
-        sum2 = sum2+1
-        if value[i] == '!':
-            value[i] = 0
-        f = float(value[i])
-        boys.append(f)
-        sum = sum + f
-print(boys)
-print(sum)
-print(sum2)
-average = sum/sum2
-average = "{:.2f}".format(average)
-print(average)
+def gender_avg():
+    bSum = 0  
+    gSum = 0
+    aSum = 0
+    bSum2 = 0  
+    gSum2 = 0
+    aSum2 = 0
+    
+    for i, val in gender.iteritems():
+        if gender[i] == "Boys":
+            if value[i] == '!':
+                value[i] = 0
+            f = float(value[i])
+        if value[i] =="Boys":
+            bSum2 = bSum2+1
+            bsum = bsum + f
+        if value[i] =="Girls":
+            gSum2 = gSum2+1
+            gSum = gSum + f
+        if value[i] =="All":
+            aSum2 = aSum2+1
+            asum = asum + f
+    print(bSum2)
+    
+    gender_avg()
