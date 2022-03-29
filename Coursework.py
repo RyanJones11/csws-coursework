159c9eb4ff196d31bb33fa6301e547f4f86e452e
wheezymccheezy00 <97961560+wheezymccheezy00@users.noreply.github.com>
2022-03-29 14:21:40 +0100

    menu function
    Added function to utilise code for user side with input validation implemented

--- Coursework.py
+++ Coursework.py
@@ -37,17 +37,30 @@ value = data["Value"]
 #                                                               #
 #                                                               #
 #################################################################
-
-
-
-
-
-
-
-
-
-
-
+def menu():
+    repeat=True
+    while repeat==True:
+        menuSelect=0
+        print("Enter a number between 1-4:\n1:Keyword search\n2:Co Efficient\n3:Total Passing\n4:Averages")
+        menuSelect=input()
+        if menuSelect == "1":
+            print("Keyword Search")
+            repeat = True
+        elif menuSelect == "2":
+            print ("Co Efficient")
+            CoefficientOfDev()
+            repeat =True
+        elif menuSelect == "3":
+            print ("Total Passing")
+            TotalPassing()
+            repeat = True
+        elif menuSelect == "4":
+            print ("Averages")
+            repeat = True
+        else:
+            print ("Invalid answer, please re enter")
+            repeat = True
+menu()
 #################################################################
 #                                                               #
 #                                                               #
