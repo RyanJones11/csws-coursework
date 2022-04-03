f14f816d4ba028b244f524aca7e2183af20a7d8c
wheezymccheezy00 <kyle.langton@outlook.com>
2022-04-03 14:09:26 +0100

    menu function
    Included exit feature to kill the program

--- Menu Function KL.py
+++ Menu Function KL.py
@@ -2,22 +2,25 @@ def menu():
     repeat=True
     while repeat==True:
         menuSelect=0
-        print("Enter a number between 1-4: ")
+        print("Enter a number between 1-4 or 5 to exit:")
         menuSelect=input()
         if menuSelect == "1":
             print("Keyword search")
             repeat = True
         elif menuSelect == "2":
             print ("Passing percentage")
-            TotalPassing()
+            #TotalPassing()
             repeat =True
         elif menuSelect == "3":
             print ("Co Efficient")
-            CoefficientOfDev()
+            #CoefficientOfDev()
             repeat = True
         elif menuSelect == "4":
             print ("Averages")
             repeat = True
+        elif menuSelect == "5":
+            print("menu exit")
+            repeat = False
         else:
             print ("Invalid answer, please reenter")
             repeat = True
